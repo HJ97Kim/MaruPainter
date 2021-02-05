f28a534dcea888e0cd2c6c6fd9cabaf67a503116
Hyeongjin Kim <kimjin5537@daum.net>
2021-02-05 13:47:34 +0900

    팔레트 애니메이션 & 배경색에 따른 색상값 color 변경

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const colorCode = document.querySelectorAll('.color_code');
 
 for (const selectColor of colors) {
   selectColor.addEventListener('click', (event) => {
-    if (colorCode[0].innerText === event.target.dataset.primary) {
+    if (colorCode[0].innerText === event.target.dataset.primary) { // 같은 색상 클릭시 초기화
       colorCode[0].innerText = '';
       colorCode[1].innerText = '';
       colorCode[2].innerText = '';
@@ -16,10 +16,23 @@ for (const selectColor of colors) {
     } else {
       mainColor.style.backgroundColor = event.target.dataset.primary;
       colorCode[0].innerText = event.target.dataset.primary;
+      colorCode[0].style.color = changeTextColor(event.target.dataset.primary);
       lightColor.style.backgroundColor = event.target.dataset.light;
       colorCode[1].innerText = event.target.dataset.light;
+      colorCode[1].style.color = changeTextColor(event.target.dataset.light);
       darkColor.style.backgroundColor = event.target.dataset.dark;
       colorCode[2].innerText = event.target.dataset.dark;
+      colorCode[2].style.color = changeTextColor(event.target.dataset.dark);
     }
   });
 };
+
+const changeTextColor = (hexcolor) => {
+  hexcolor = hexcolor.replace("#", "");
+  console.log(hexcolor);
+  const r = parseInt(hexcolor.substr(0,2),16);
+  const g = parseInt(hexcolor.substr(2,2),16);
+  const b = parseInt(hexcolor.substr(4,2),16);
+  const yiq = ((r*299)+(g*587)+(b*114))/1000;
+  return (yiq >= 128) ? 'black' : 'white'; // 어두우면 white 밝으면 black
+}
